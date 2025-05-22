43a794fca2c0fef99a5daab2cfb1df865eec2746
harijp11 <harijayaprakash10@gmail.com>
2025-05-23 00:04:46 +0530

    modified:   src/common/maping/LocationPickerComponent.tsx v_14

--- src/Components/user/Purse/Purse.tsx
+++ src/Components/user/Purse/Purse.tsx
@@ -18,7 +18,7 @@ import { usePurseQuery, useAddMoneyMutation, useConfirmPaymentMutation } from '@
 import { PaymentResult } from "./paymentResult"
 
 const stripePromise = loadStripe('pk_test_51RIAruRtuCub2T27DnxxEqDHZNN1Y9y3mKkkSGS6ppflKtMBH202fZBlkPdlJtnrhDNkXOn2I5lsRRckG4MAvYR600K0Oosioa')
- export const UserPurse = () => {
+ const Purse = () => {
   const [isAddMoneyOpen, setIsAddMoneyOpen] = useState(false)
   const [paymentAmount, setPaymentAmount] = useState<string>("")
   
@@ -533,3 +533,4 @@ function PurseSkeleton() {
   )
 }
 
+export default Purse
