2939b5238fcc4fda12a33f75ebe82300742080bf
harijp11 <harijayaprakash10@gmail.com>
2025-05-23 00:22:45 +0530

    modified:   src/common/maping/LocationPickerComponent.tsx v_16

--- src/pages/user/PursePage.tsx
+++ src/pages/user/PursePage.tsx
@@ -1,6 +1,6 @@
 // import Footer from "@/Components/footer/userFooter";
 import { UserHeader } from "@/Components/headers/userHeader";
-import Purse from "@/Components/user/purse/Purse";
+// import Purse from "@/Components/user/purse/Purse";
 
 
 
@@ -14,7 +14,7 @@ function UserPursePage() {
 
       <main className="flex-grow">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 -py-8">
-         <Purse/>
+         {/* <Purse/> */}
         </div>
       </main>
 
